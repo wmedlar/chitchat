6c008a1506d204715a2859fe37f0e995a3f4e62e
necromanteion <wmmedlar@gmail.com>
2015-10-15 13:22:12 -0500

    additional documentation

--- chitchat/__init__.py
+++ chitchat/__init__.py
@@ -1,5 +1,6 @@
 import asyncio
 import collections
+import typing
 
 from chitchat.connection import Connection
 from chitchat import irc
@@ -7,38 +8,77 @@ from chitchat.mixins import CommandMixin
 
 
 class Client(Connection, CommandMixin):
-    '''
 
-    '''
+
     actions = collections.defaultdict(set)
 
 
-    async def run(self):
+    async def run(self) -> None:
+        '''
+        The main method of the Client class, run within an event loop to connect to an IRC server and iterate over
+        messages received.
+        '''
 
         await self.connect()
 
         while self.connected:
 
+            # async for loop will yield a line as it is received
             async for line in self.reader:
+                # irc.Message object allows lazily-evaluated attribute access
+                # offers a speedup in some cases compared to eager evaluation of parameters
+                message = irc.Message(line.decode())
 
-                message = irc.parse(line.decode())
+                # async triggered actions will return immediately
+                # sync triggered actions will block until completion
                 await self.trigger(message)
 
             # disconnect in the loop allows the client to reconnect without returning
             await self.disconnect()
 
 
-    def on(self, command, *commands, async=False):
+    def on(self, command: str, *commands, async=False):
+        '''
+        A function decorator to register a listener called when a message of type `command` is received, with optional
+        blocking and nonblocking calls.
+
+        The decorated function should take only one parameter, an irc.Message object containing lazily-parsed
+        parameters in its attributes.
+
+        This decorator uses a function attribute to qualify a blocking or nonblocking call, thus the `async` attribute
+        of the function must be available to the decorator for use.
+
+        Args:
+            command: The string IRC command or three-digit numeric that will trigger the decorated function. At least
+                     one command is required.
+            *commands: Additional commands or numerics used to trigger the decorated function.
+            async: Boolean keyword-only argument used to specify that this function is to be run as nonblocking.
+
+        Returns:
+            The decorated function wrapped in asyncio.coroutine.
+        '''
 
         commands = command, *commands
 
-        def wrapper(func):
+        def wrapper(func: typing.Callable[[irc.Message], typing.Any]) -> typing.Callable[[irc.Message], typing.Any]:
+            '''
+            Wraps the decorated function in a coroutine and appends it to self.actions for each command passed.
 
+            Args:
+                func: The function to be decorated.
+
+            Returns:
+                The decorated function wrapped in asycio.coroutine.
+            '''
+
+            # function must be a coroutine to be yielded from
             if not asyncio.iscoroutine(func):
                 func = asyncio.coroutine(func)
 
+            # async attribute is used in self.trigger to determine a blocking or nonblocking call
             func.async = async
 
+            # allows multiple commands to trigger the function
             for command in commands:
                 self.actions[command].add(func)
 
@@ -64,6 +104,7 @@ class Client(Connection, CommandMixin):
                 asyncio.ensure_future(action(message))
 
             else:
+                # synchronous calls will block execution of asynchronous calls
                 yield from action(message)
 
 
@@ -71,7 +112,6 @@ if __name__ == '__main__':
     import os
 
     client = Client(host='irc.rizon.net', port=6667, ssl=False)
-    print(client.encoding)
 
     @client.on('NOTICE')
     def dostuff(message):
@@ -90,20 +130,6 @@ if __name__ == '__main__':
     @client.on(*irc.ALL, async=True)
     def log(message):
         print(message.raw)
-
-    @client.on('PRIVMSG')
-    def test(message):
-        if message.nick == 'necromanteion' and message.params[1] == 'sync':
-            for i in range(3):
-                client.msg('#Sakubot', message='sync! {}'.format(i))
-                yield from asyncio.sleep(1)
-
-    @client.on('PRIVMSG', async=True)
-    def test2(message):
-        if message.nick == 'necromanteion' and message.params[1] == 'async':
-            for i in range(10):
-                print('async! {}'.format(i))
-                yield from asyncio.sleep(0.5)
 
 
     loop = asyncio.get_event_loop()
