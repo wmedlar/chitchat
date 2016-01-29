ec577e1660fa42ad040581494213dae1d1edfd82
necromanteion <wmmedlar@gmail.com>
2016-01-29 17:39:39 -0600

    Connection.__init__ rewritten to accept loop kwarg, host, port, and **kwargs moved to Connection.connect

--- chitchat/connection.py
+++ chitchat/connection.py
@@ -1,88 +1,84 @@
-import abc
 import asyncio
 
-from .constants import CONNECTED, DISCONNECTED
-from .utils import prep
-    
+from . import constants, utils
+
+# TODO: move Connection.run to Client
 
 class Connection:
     
     
-    def __init__(self, host, port, *, encoding='UTF-8', **kwargs):
-        self.host = host
-        self.port = port
+    def __init__(self, *, encoding='UTF-8', loop=None):
         self.encoding = encoding
-        self.kwargs = kwargs
+        self.loop = loop or asyncio.get_event_loop()
         
         self.connected = False
         self.reader = None
         self.writer = None
         
     
-    async def connect(self, *, loop=None):
+    async def connect(self, host, port, **kwargs):
         """Open a connection to the host."""
         
-        loop = loop or asyncio.get_event_loop()
+        self.reader, self.writer = await asyncio.open_connection(host, port,
+                                                                 loop=self.loop, **kwargs)
         
-        self.reader, self.writer = await asyncio.open_connection(self.host, self.port, loop=loop, **self.kwargs)
         self.connected = True
-
-        # mimic recieving a 'CONNECTED' command - a pseudocommand to signify that we have connected to the server
-        await self.handle(prep(CONNECTED, self.encoding))
-    
+        host_ip, host_port = self.writer.get_extra_info('peername')
+        
+        # pseudocommand signifying a successful connection
+        line = utils.ircjoin(':' + host_ip, constants.CONNECTED, host_port).encode(self.encoding)
+        
+        # feed the pseudocommand to the StreamReader
+        self.reader.feed_data(line)
 
 
     async def disconnect(self):
-        """Disconnect from server and clean up transport."""
+        """Disconnect from server."""
 
         self.connected = False
+        host_ip, host_port = self.writer.get_extra_info('peername')
 
-        # mimic recieving a 'DISCONNECTED' command - a pseudocommand to signify that we have been disconnected from the server
-        await self.handle(prep(DISCONNECTED, self.encoding))
+        # pseudocommand signifying a closed or lost connection from the server
+        line = utils.ircjoin(':' + host_ip, constants.DISCONNECTED, host_port).encode(self.encoding)
+        
+        # iteration over the reader has stopped, so we'll send this one to self.handle
+        await self.handle(line)
     
 
     async def handle(self, line):
         """
-        Message handler called by `self.run`. This method exists to be overridden in subclasses
-        and does nothing by default.
+        Message handler called by `self.run`. This method exists to be overridden in
+        subclasses and does nothing by default.
         """
 
         pass
-    
-    
-    async def prepare(self, line):
-        """
-        Prepares a string to be sent to the transport stream. By default this method simply encodes the string to a
-        bytes object using the supplied encoding.
-        """
-        return line.encode(self.encoding)
-    
 
 
-    async def run(self, *, loop=None):
+    async def run(self):
         """
-        The main method of the Connection class, run within an event loop to connect to an IRC server and iterate over
-        messages received.
+        The main method of the Connection class, run within an event loop to connect to an
+        IRC server and iterate over messages received.
         """
 
-        await self.connect(loop=loop)
+        await self.connect()
 
         while self.connected:
             
             async for line in self.reader:
+                
                 await self.handle(line)
 
             # disconnect inside the loop allows the client to reconnect without returning
             await self.disconnect()
 
 
-    async def send(self, lines):
+    def send(self, lines):
         """
         Prepares and writes messages to the connected server.
 
         args:
-            lines: An iterable of string or bytes objects representing the messages to write. Strings will be
-                   passed to `self.prepare` before being written to the transport stream.
+            lines: An iterable of string or bytes objects representing the messages to
+            write.
 
         returns:
             None
@@ -94,9 +90,4 @@ class Connection:
         if not self.writer:
             raise RuntimeError('connection is not ready')
         
-        for line in lines:
-            
-            if isinstance(line, str):
-                line = await self.prepare(line)
-                
-            self.writer.write(line)
+        self.writer.write(lines)
