52e8d7217239b56617476e7fc6b87906eb67f901
necromanteion <wmmedlar@gmail.com>
2016-05-26 21:06:55 -0500

    added Message as successor to prefix class for abstracting server messages, added Plugin as successor to Callback class for wrapping callback functions, deprecated prefix, Plugin, and Validate

--- chitchat/structures.py
+++ chitchat/structures.py
@@ -1,96 +1,136 @@
 import asyncio
 import collections
-import functools
 
 from . import utils
 
 
-class Callback:
-    """
-    A callback triggered upon receiving messages from the connected server.
-    """
+class Plugin:
     
     
-    def __init__(self, func, validator):
+    def __init__(self, func, command, **kwargs):
         self.func = func
-        self.validator = validator
-        
-        self.awaitable = asyncio.iscoroutinefunction(func)
-    
-    
-    async def __call__(self, *args):
-        
-        if not self.validate(*args):
-            return False
-        
-        if self.awaitable:
-            result = await self.func(*args)
-        
-        else:
-            result = self.func(*args)
-            
-        return result
-    
-    
-    def validate(self, *args):
-        """
-        Determines whether passed arguments are sufficient to trigger the wrapped
-        function.
-        
-        This will return False at the first opportunity to indicate that the supplied
-        arguments are insufficient and True if all conditions are met.
-        """
-        
-        return self.validator(*args)
+        self.command = command
     
         
+    @property
+    def func(self):
+        return self._func
+    
+    
+    @func.setter
+    def func(self, value):
+        self._func = asyncio.coroutine(value)
+    
+        
+    async def __call__(self, *args, **kwargs):            
+        val = await self.func(*args, **kwargs)
+        return val
+    
+    
     def __repr__(self):
-        line = '{0.__class__.__name__}(func={0.func.__name__}, validator={0.validator!r})'
-        return line.format(self)
-    
-    
-def Validator(*, nick=True, user=True, host=True, target=True, text=True):
-    
-    funcs = (
-        utils.as_comparable(nick),
-        utils.as_comparable(user),
-        utils.as_comparable(host),
-        utils.as_comparable(target),
-        utils.as_comparable(text)
-    )
-    
-    @functools.lru_cache(maxsize=128)
-    def validate(prefix_, target_, text_=None):
-        
-        params = (*utils.prefixsplit(prefix_), target_, text_)
-            
-        return all(func(arg) for func, arg in zip(funcs, params))
-    
-    rep = 'Validator(nick={0}, user={1}, host={2}, target={3}, text={4})'
-    validate.__repr__ = rep.format(nick, user, host, target, text)
-    
-    return validate
-    
-    
+        r = '{0.__class__.__name__}(func={0.func})'
+        return r.format(self)
 
-class prefix(str):
-    """
-    A string subclass with nick, user, and host attributes and no other added functionality.
+
+class Message(str):
     
-    All methods will operate on the value passed to the constructer, and will not return a
-    new instance of `prefix`.
-    """
     
-    __slots__ = ('nick', 'user', 'host')
+    @property
+    def raw(self):
+        """Underlying string message from the server. Alias for `str(self)`."""
+        return str(self)
+
+
+    @utils.lazyproperty
+    def parsed(self):
+        """Message parsed into component prefix, command, and params."""
+        return utils.ircparse(self)
     
-    def __new__(cls, ircprefix):
-        inst = super().__new__(cls, ircprefix)
-        inst.nick, inst.user, inst.host = utils.prefixsplit(ircprefix)
-        return inst
+    
+    @utils.lazyproperty
+    def parsed_prefix(self):
+        """Prefix of the message sender parsed into component nick, user, and host."""
+        return utils.prefixsplit(self.prefix)
+    
+    
+    @property
+    def prefix(self):
+        """Prefix of the message sender. Alias for `self.parsed.prefix`."""
+        return self.parsed.prefix
+    
+    
+    @property
+    def command(self):
+        """Message command or numeric reply. Alias for `self.parsed.command`."""
+        return self.parsed.command
+    
+    
+    @property
+    def params(self):
+        """Tuple of message parameters. Alias for `self.parsed.params`."""
+        return self.parsed.params
+    
+    
+    @property
+    def nick(self):
+        """Nickname of message sender. Alias for `self.parsed_prefix.nick`."""
+        return self.parsed_prefix.nick
+    
+    
+    @property
+    def user(self):
+        """Username of message sender. Alias for `self.parsed_prefix.user`."""
+        return self.parsed_prefix.user
+    
+    
+    @property
+    def host(self):
+        """Hostname of message sender. Alias for `self.parsed_prefix.host`."""
+        return self.parsed_prefix.host
+    
+    
+    @property
+    def target(self):
+        """Message target channel or user."""
+        # target will be the first parameter for *most* messages
+        try:
+            target = self.params[0]
+        except IndexError:
+            target = None
+            
+        return target
+    
+    
+    @property
+    def channel(self):
+        """Message target channel, or None if target is not a channel."""
+        target = self.target
+        return target if target and utils.ischannel(target) else None
+    
+    
+    def reply_to(self, public=True):
+        """
+        Determines the target to reply to.
         
+        args:
+            public (bool): if True reply publicly (i.e., in channel) if possible,
+                           defaults to True
+        returns:
+            str channel or nick to reply to
+        
+        """
+        channel = self.channel
+    
+        if public and channel:
+            return channel
+        
+        # if no nick can be identified then this is probably from a server
+        return self.nick or self.host
+    
+    
     def __repr__(self):
-        line = '{0.__class__.__name__}({0}, nick={0.nick!r}, user={0.user!r}, host={0.host!r})'
-        return line.format(self)
+        r = '{0.__class__.__name__}({0.raw!r})'
+        return r.format(self)
     
     
 class CaseInsensitiveDefaultDict(collections.abc.MutableMapping):
